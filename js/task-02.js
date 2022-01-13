75713a5065dbd329eb1094d1d293fa5358d8ee1f
Maksym Bernatskyi <bernasik007@gmail.com>
2022-01-14 00:48:57 +0200

    task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const allIngredientsList = document.querySelector('#ingredients');
+
+const ingredientsItem = ingredients.map(ingredient => {
+  const ingredientsElement = document.createElement('li');
+  ingredientsElement.classList.add('ingredients__item');
+  ingredientsElement.textContent = ingredient;
+  
+  return ingredientsElement;
+});
+
+allIngredientsList.append(...ingredientsItem);
+
+console.log(allIngredientsList);
