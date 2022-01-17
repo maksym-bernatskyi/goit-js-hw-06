a430c965ca484312cb3ea37c5015ec5f8c3518da
Maksym Bernatskyi <bernasik007@gmail.com>
2022-01-17 09:58:29 +0200

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonChangeColor = document.querySelector('.change-color');
+const textColor = document.querySelector('.color');
+
+function onButtonClick() {
+  textColor.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+buttonChangeColor.addEventListener('click', onButtonClick);
