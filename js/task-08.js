e9ab1de116c7e01cac2cd72d3e1284ecfc24eaba
Maksym Bernatskyi <bernasik007@gmail.com>
2022-01-17 09:48:45 +0200

    task-08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', dataSubmit);
+
+function dataSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        alert('Все поля должны быть заполнены!')
+    }
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
